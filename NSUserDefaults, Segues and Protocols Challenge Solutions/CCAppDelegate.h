625c738d2ee18598fd10cea7653ca6a242253059
Rob Lipp <lunarwild@gmail.com>
2015-09-24 12:34:42 -0700

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solutions/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  NSUserDefaults, Segues and Protocols Challenge Solutions
+//
+//  Created by Rob Lipp on 2015-09-24.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Challenge Solutions/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solutions
+//
+//  Created by Rob Lipp on 2015-09-24.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
